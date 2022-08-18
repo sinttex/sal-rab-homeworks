acc624ca88aa46db906f5612b74186f27e64a549
sinttex <109655929+sinttex@users.noreply.github.com>
2022-08-18 17:19:20 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,7 +1,7 @@
 function addInCartMessage(title, price) {
-    let productName = title;
-    let productPrice = price;
-
+    //let productName = title;
+    //let productPrice = price;
+    let message = title + ' за ' + price + ' теперь в корзине!'
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
