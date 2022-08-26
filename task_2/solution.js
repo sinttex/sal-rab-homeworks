ac29a348f991d298c2f55cbbe8f610ab60dd6a80
sinttex <109655929+sinttex@users.noreply.github.com>
2022-08-26 12:38:27 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,6 +2,7 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+    
     let shippingSum;
     if (productsSum >= freeShippingMinSum) {
         shippingSum = 0;
@@ -34,6 +35,7 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
+    
     let discountSum;
     if (productsSum >= discountMinSum) {
         discountSum = productsSum * discountPart / 100;
@@ -54,6 +56,7 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    
     let totalSum;
     totalSum = productsSum;
     totalSum = totalSum - discountSum;
@@ -65,6 +68,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+    
     totalSum = totalSum + shippingSum;
     let freeShipping;
     shippingSum == 0 ? freeShipping = true : freeShipping = false;  
